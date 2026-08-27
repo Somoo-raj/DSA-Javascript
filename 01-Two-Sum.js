2ab20bbee896a5e940d819a8401b0ea705c1b075
Sonu Kumar <148649438+Somoo-raj@users.noreply.github.com>
2026-08-27 15:35:50 +0530

    Enhance comments for clarity in Two Sum function
    Added comments to clarify the purpose of the code.

--- 01-Two-Sum.js
+++ 01-Two-Sum.js
@@ -7,11 +7,12 @@ var twoSum = function (nums, target) {
 
     for (let i = 0; i < nums.length; i++) {
         let complement = target - nums[i];
-
+        
+        // if the complement is found in the map.
         if (map.has(complement)) {
             return [map.get(complement), i];
         }
-
+        // store the value and its index.
         map.set(nums[i], i);
     }
 };
